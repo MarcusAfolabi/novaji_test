35dceaec1f86a44b9a31db3393b1b9a5c4f1e84b
MarcusAfolabi <biormarcus@gmail.com>
2023-05-18 16:14:52 +0100

    product migration table updated

--- database/migrations/2023_05_18_092942_create_products_table.php
+++ database/migrations/2023_05_18_092942_create_products_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('slug');
+            $table->string('views');
+            $table->string('status');
             $table->string('image');
             $table->text('description');
             $table->decimal('price', 8, 2); 
